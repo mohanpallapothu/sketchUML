cc95ab511c67a72f960330e393ad39f4b059ecdf
ASUAD\vkumarpa <vkumarpa@asu.edu>
2024-02-25 18:09:44 -0700

    Save and load user selections

---
+++ src/Model/FileManager.java
@@ -0,0 +1,72 @@
+package Model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * This class access the user class data and does the file load or save.
+ */
+public abstract class FileManager {
+
+    private static String path = "";
+    private static String SPACE = " ";
+    public static boolean validateFileExist(String file) {
+        File myFile = new File(path+file);
+        if (myFile.exists()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * This method saves all the user data into the file given by the user.
+     * @param file
+     */
+    public static void save(String file) {
+        try {
+            FileWriter myWriter = new FileWriter(path+file);
+            DrawnClasses drawnClasses = DrawnClasses.getInstance();
+            for (UserClass userClass: drawnClasses.getClasses()) {
+                myWriter.write("n" + SPACE + userClass.getTitle() + SPACE + userClass.xCoord() + SPACE + userClass.yCoord() + '\n');
+            }
+            for (int i=0; i<drawnClasses.getLength(); i++) {
+                UserClass userClass = drawnClasses.getClassByID(i);
+                for (Connection connection: userClass.getConnections()) {
+                    myWriter.write("c" + SPACE + connection.getType().name + SPACE + i + SPACE + connection.getToID() + '\n');
+                }
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method loads all the user data from the file mentioned by the user.
+     * @param file
+     */
+    public static void load(String file) {
+        try {
+            File myFile = new File(path+file);
+            Scanner myReader = new Scanner(myFile);
+            DrawnClasses drawnClasses = DrawnClasses.getInstance();
+            drawnClasses.reset();
+            while (myReader.hasNextLine()) {
+                String d = myReader.nextLine();
+                String[] a = d.split(" ");
+                if (a[0].charAt(0) == 'n') {
+                    drawnClasses.addUserClass(Integer.parseInt(a[2]), Integer.parseInt(a[3]), a[1]);
+                } else if (a[0].charAt(0) == 'c') {
+                    System.out.println(a[1]);
+                    drawnClasses.addConnection(Integer.parseInt(a[2]), Integer.parseInt(a[3]), ConnectionType.valueOf(a[1]));
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
