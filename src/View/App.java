4f0339b2b86a3e252370ac76d0dae571cfdcb39c
ASUAD\vkumarpa <vkumarpa@asu.edu>
2024-03-10 23:40:38 -0700

    Move the files from Controller to View

--- src/View/App.java
+++ src/View/App.java
@@ -1,5 +1,6 @@
 package View;
 
+import Controller.AppController;
 import Model.*;
 
 import javax.swing.*;
@@ -30,11 +31,55 @@ public class App extends JFrame {
         JMenuItem member_2 = new JMenuItem(TeamList.CHINMAY.name);
         JMenuItem member_3 = new JMenuItem(TeamList.SNIGDHA.name);
         JMenuItem member_4 = new JMenuItem(TeamList.MOHAN.name);
+        CodePanel codePanel;
+        DesignPanel designPanel;
+        StatusBar statusBar = new StatusBar(-2, 745, ViewConstants.panelWidth+4, 37);
         menuBar = new JMenuBar();
         JMenu file = new JMenu("File");
         JMenu help = new JMenu("Help");
         JMenu connectionType = new JMenu("Connection");
+        AppController appController = new AppController();
+        save.addActionListener(appController);
+        load.addActionListener(appController);
+        clear.addActionListener(appController);
+        association.addActionListener(appController);
+        inheritance.addActionListener(appController);
+        aggregation.addActionListener(appController);
+        file.add(save);
+        file.add(load);
+        file.add(clear);
+        help.add(member_1);
+        help.add(member_2);
+        help.add(member_3);
+        help.add(member_4);
+        connectionType.add(association);
+        connectionType.add(inheritance);
+        connectionType.add(aggregation);
+        menuBar.add(file);
+        menuBar.add(help);
+        menuBar.add(connectionType);
+        menuBar.setBounds(-2,-2,ViewConstants.panelWidth+4,35);
+        menuBar.setBorder(BorderFactory.createLineBorder(ViewConstants.accentColor, 2));
+        menuBar.setBackground(Color.white);
+        codePanel = new CodePanel(10, 45, 300, 690);
+        codePanel.setBounds(10,45,300,690);
+        codePanel.setBorder(BorderFactory.createLineBorder(ViewConstants.accentColor, 2));
+        codePanel.setBackground(Color.white);
+        designPanel = new DesignPanel(320, 45, 670, 690);
+        GlobalStatus.getInstance().addObserver(statusBar);
+        DrawnClasses.getInstance().addObserver(codePanel);
+        DrawnClasses.getInstance().addObserver(designPanel);
 
+        this.setPreferredSize(new Dimension(ViewConstants.panelWidth,ViewConstants.panelHeight));
+        this.setLayout(null);
+        this.add(menuBar);
+        this.add(codePanel);
+        this.add(designPanel);
+        this.add(statusBar);
+        this.add(new Panel());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
     }
 
     public static void main(String[] args) {
